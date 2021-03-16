4b537e18412a11de6cd7d8d44239e1d823859d48
Adam Dewberry <38838996+AdamDewberry@users.noreply.github.com>
2021-03-16 19:24:06 +0000

    Refine readme and requirements, change snowflake connection engine for dataframe upload, add pre-commit linter

--- Snowflake_Connector.py
+++ Snowflake_Connector.py
@@ -2,7 +2,8 @@ import snowflake.connector
 from snowflake.connector import DictCursor
 import pandas as pd
 
-class Snowflake_Connector:
+
+class SnowflakeConnector:
     def __init__(self, connection_parameters):
         self.connection_parameters = connection_parameters
 
@@ -12,7 +13,8 @@ class Snowflake_Connector:
             return connection
         except Exception as Error_returned:
             raise RuntimeError(
-            "Snowflake Connection", f"Error connecting to Snowflake: {Error_returned}"
+                "Snowflake Connection",
+                f"Error connecting to Snowflake: {Error_returned}",
             )
 
     def set_session_parameters(self, role: str, warehouse: str):
@@ -23,31 +25,32 @@ class Snowflake_Connector:
             return cursor
         except Exception as error_returned:
             raise RuntimeError(
-            f"Setting the Role and Warehouse threw error: {error_returned}"
+                f"Setting the Role and Warehouse threw error: {error_returned}"
             )
 
-    def run_sql(
-        self, cursor: snowflake.connector.DictCursor,
-        sql_statements: str
-        ):
+    def run_sql(self, cursor: snowflake.connector.DictCursor, sql_statements: str):
         try:
             cursor.execute(sql_statements)
             rows_returned = [row for row in cursor]
             return rows_returned
         except Exception as error_returned:
             raise RuntimeError(
-            f"SQL statements: {sql_statements}\n threw error {error_returned}"
+                f"SQL statements: {sql_statements}\n threw error {error_returned}"
             )
 
-    def fetch_dataframe_from_sql(self, cursor:snowflake.connector.cursor.DictCursor, sql_query: str):
+    def fetch_dataframe_from_sql(
+        self, cursor: snowflake.connector.cursor.DictCursor, sql_query: str
+    ):
         try:
             query_result = cursor.execute(sql_query)
-            df = pd.DataFrame.from_records(iter(query_result), columns = [row[0] for row in query_result.description])
+            df = pd.DataFrame.from_records(
+                iter(query_result), columns=[row[0] for row in query_result.description]
+            )
             return df
         except Exception as error_returned:
             raise RuntimeError(
-            f"SQL statement: {sql_query}\n threw error: {error_returned}"
+                f"SQL statement: {sql_query}\n threw error: {error_returned}"
             )
 
     def closer_cursor(self, cursor):
-        cursor.close()         
+        cursor.close()
